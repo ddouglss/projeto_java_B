6bb478b5a1aa0c66320f1ae8def8986e2b9d3235
ddouglss <ddouglss1999@gmail.com>
2025-04-02 21:30:45 -0300

    Desenvolvendo as Entiidades no projeto

---
+++ src/main/java/com/initial/bookstoremanager/entity/Author.java
@@ -0,0 +1,23 @@
+package com.initial.bookstoremanager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+//MAPEANDO CONFORME O MODELO DE DADOS E COM AS ANNOTATIONS
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
